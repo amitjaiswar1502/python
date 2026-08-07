199157bfe1278d828b1fc88f073d4ba82200b4e5
amitjaiswar1502 <121933841+amitjaiswar1502@users.noreply.github.com>
2026-08-07 10:15:19 +0530

    python practice set

---
+++ python-practice/practice-set/ps_1.py
@@ -0,0 +1,30 @@
+# 1. Write a program to print Twinkle twinkle little star poem in python.
+# print("Twinkle, twinkle, little star,")
+# print("How I wonder what you are!")
+
+# print("Twinkle, twinkle, little star, \nHow I wonder what you are! \nUp above the world so high, \nLike a diamond in the sky.")
+
+
+# 2. Use REPL and print the table of 5 using it.
+
+
+
+# 3. Install an external module and use it to perform an operation of your interest.
+ #install flask or django : python3 install flask
+
+
+
+# 4. Write a python program to print the contents of a directory using the os module. Search
+# import os
+# cwd = os.getcwd()
+# print("print the CWD" ,cwd)
+
+
+# 4. Write a python program to print the contents of a directory using the os module. Search
+# online for the function which does that
+# import os
+# list = os.listdir()
+# print(list)
+
+
+# 5. Label the program written in problem 4 with comments.
---
+++ python-practice/practice-set/ps_2.py
@@ -0,0 +1,29 @@
+# 1. Write a python program to add two numbers.
+# variant 1
+# num1 = 4
+# num2 = 8
+# result = num1 + num2
+# print(result)
+
+# variant 2
+# num1 = float(input("Enter first number"))
+# num2 = float(input("Enter second number"))
+# result = num1 + num2
+# print(type(result))
+# print(result)
+
+# print(5+4)
+
+
+# 2. Write a python program to find remainder when a number is divided by z.
+num1 = 5
+num2 = 2
+print("remainder is: ", num1%num2)
+
+
+
+# 3. Check the type of variable assigned using input() function.
+# 4. Use comparison operator to find out whether ‘aʼ given variable is greater than ‘bʼ or not.
+# Take a = 34 and b = 80
+# 5. Write a python program to find an average of two numbers entered by the user.
+# 6. Write a python program to calculate the square of a number entered by the user
