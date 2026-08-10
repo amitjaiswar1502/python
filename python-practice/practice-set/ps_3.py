85022bb41f2924180f4279a0db532a1d61eaec80
amitjaiswar1502 <121933841+amitjaiswar1502@users.noreply.github.com>
2026-08-10 10:05:34 +0530

    ps 4 done

---
+++ python-practice/practice-set/ps_3.py
@@ -0,0 +1,38 @@
+# 1. Write a program to store seven fruits in a list entered by the user.
+
+# fruits = []
+# for f in range(1,8):
+#     fruit = input(f"Enter the {f} ")
+#     fruits.append(fruit)
+# print(fruits)
+
+
+
+# 2. Write a program to accept marks of 6 students and display them in a sorted manner.
+# marks = []
+# for m in range(1,6):
+#     mark = input(f"Enter the marks {m} ")
+#     marks.append(mark)
+#     marks.sort()
+
+# print(marks)
+
+
+
+
+# 3. Check that a tuple type cannot be changed in python.
+# t1 =(1, 2.3,4 , 5, 6,6,6,6)
+# res = t1.append(22)
+# print(res)
+
+# 4. Write a program to sum a list with 4 numbers.
+# nums = [34,54,66,33]
+# result = sum(nums)
+# print(result)
+
+
+# 5. Write a program to count the number of zeros in the following tuple:
+
+# a = (7, 0, 8, 0, 0, 9)
+# output = a.count(0)
+# print(output)
---
+++ python-practice/practice-set/ps_4.py
@@ -0,0 +1,71 @@
+# 1. Write a program to create a dictionary of Hindi words with values as their English translation. Provide user with an option to look it up!
+# dict = { 
+#         "namaste":"hello",
+#          "prabhat":"morning",
+#         "nashta": "breakfast"
+#          }
+# user = input("Enter the hindi word : ")
+# res = dict.get(user)
+
+# print(res)
+
+
+# 2. Write a program to input eight numbers from the user and display all the unique numbers (once).
+# nums = []
+# for i in range(1,8):
+#     user = int(input(f"Enter the {1} number : "))
+#     nums.append(user)
+#     s1 = set(nums)
+# print(s1)
+
+
+# 3. Can we have a set with 18 (int) and '18' (str) as a value in it?
+# s2 = { 18 , "18"}
+# print(s2)
+
+
+
+# 4. What will be the length of following set s:
+# s = set()
+# s.add(20)
+# s.add(20.0)
+# s.add('20') # length of s after these operations?
+# res =s.len()
+# print(res)
+
+
+# 5. What is the type of 's'?
+# s = {}
+# print(type(s))
+
+# 6. Create an empty dictionary. Allow 4 friends to enter their favorite language as value and use key as their names. Assume that the names are unique.
+# student_subjects = {}
+# names = [ "Amit", "Ajeet", "Arijit","Sukesh"]
+# for i in names:
+#     subject = input(f"Enter the subject for {i} : ")
+#     student_subjects[i] = subject
+# print(type(student_subjects))
+
+
+
+
+
+# 7. If the names of 2 friends are same; what will happen to the program in problem 6?
+# student_subjects = {}
+# names = [ "Amit", "Amit", "Arijit","Sukesh"]
+# for i in names:
+#     subject = input(f"Enter the subject for {i} : ")
+#     student_subjects[i] = subject
+# print(student_subjects)
+
+
+# 8. If languages of two friends are same; what will happen to the program in problem 6?
+# student_subjects = {}
+# names = [ "Amit", "Ajeet", "Arijit","Sukesh"]
+# for i in names:
+#     subject = input(f"Enter the subject for {i} : ")
+#     student_subjects[i] = subject
+# print(student_subjects)
+
+# 9. Can you change the values inside a list which is contained in set S?
+# NO
