bb9e25a0415e5c07143704102d3feb7620c04f87
amitjaiswar1502 <121933841+amitjaiswar1502@users.noreply.github.com>
2026-09-17 09:39:53 +0530

    daye 1 done

---
+++ 100_days_challenge/day_1.py
@@ -0,0 +1,13 @@
+# name = "Amit"
+# print(name)
+
+
+# name = "Jaiswar"
+# print(len(name))
+
+# band name generator
+pet = input("Enter yiour pet name? :")
+city = input("Enter your city name? :")
+
+bandname = pet + city
+print(f"Your band name is :{bandname}")
